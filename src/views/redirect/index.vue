6d09a307f105162e9780bae30deb001bc0926094
xiaokebin <728680839@qq.com>
2024-11-14 17:49:08 +0800

    首次提交

---
+++ src/views/redirect/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+const { params, query } = route;
+const { path } = params;
+
+console.log("redirect.index");
+console.log("params", params);
+console.log("query", query);
+console.log("path", path);
+
+router.replace({ path: "/" + path, query });
+</script>
